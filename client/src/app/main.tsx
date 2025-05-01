3ee827893375b7f2e5a20f0d217fd6a662f85921
nickxsy <104327959+nickozy@users.noreply.github.com>
2025-05-02 02:22:18 +0300

    upd: fix filename

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './Rename';
+import { App } from './app';
 import { AppProvider } from './app-provider';
 
 createRoot(document.getElementById('root')!).render(
